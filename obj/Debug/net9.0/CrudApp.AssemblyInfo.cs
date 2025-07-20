6f29292c41b60f9a2b2aaace122e9026f60179a8
Elwan6896 <aelwan6896@gmail.com>
2025-07-21 00:13:06 +0300

    Privacy

--- obj/Debug/net9.0/CrudApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrudApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b16483cddf42e3ee5952b033813a4f9e791b2218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0bb3b6f26f61a803c31748d9dcb9cca05679de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
