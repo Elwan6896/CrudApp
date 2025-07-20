5eb1d4013a91d534f83657d34253dfb1f00d546e
Elwan6896 <aelwan6896@gmail.com>
2025-07-21 00:26:35 +0300

    table

--- obj/Debug/net9.0/CrudApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrudApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f29292c41b60f9a2b2aaace122e9026f60179a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+180b834ce03ea05d1195acea00eb9a5fbf6f332c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
